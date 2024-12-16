126d849e501648d67fe19e80bdb55f0f66aacf82
Yorqin11 <yorqinturgunboyev5@gmail.com>
2024-12-16 17:22:35 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const name = prompt('Ismingizni kiriting'); // Prompt for name
+const id = prompt('Space idingizni kiriting'); // Prompt for ID
+const password = prompt('Space passwordingizni kiriting'); // Prompt for password
+
+const user = {
+    name: name,
+    id: id,
+    password: password
+};
+
+console.log(user);
